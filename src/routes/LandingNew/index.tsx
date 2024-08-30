ba37aff1d6c5bfc8bc7d7255bebad66c17e4ee6e
sdjkfnsdjhdsuf <nurbhyt01@gmail.com>
2024-08-30 10:35:56 +0500

    wp number again ads

--- src/routes/LandingNew/index.tsx
+++ src/routes/LandingNew/index.tsx
@@ -222,7 +222,7 @@ function Landing() {
   const handleOrder = () => {
     const message = `Здравствуйте! Я по поводу книги, можете проконсультировать?`;
     const encodedMessage = encodeURIComponent(message);
-    const whatsappUrl = `https://wa.me/77751716068?text=${encodedMessage}`;
+    const whatsappUrl = `https://wa.me/77476738427?text=${encodedMessage}`;
     window.open(whatsappUrl, "_blank");
   };
 
