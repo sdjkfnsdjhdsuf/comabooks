9bea7bb035f6e1ca3d5d3c8f4d5fe9056619a34f
sdjkfnsdjhdsuf <nurbhyt01@gmail.com>
2025-03-27 19:51:43 +0500

    finished landing new

--- src/index.tsx
+++ src/index.tsx
@@ -2,52 +2,22 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
-import Forms from "./routes/Forms";
-import Home from "./routes/Home";
-import axios from "axios";
-import { serviceOptions } from "generated";
-import { Provider } from "react-redux"; // Импорт Provider
-import { store } from "./store"; // Импорт store
-import { CoverPage } from "routes/CoverPage";
-import { TokenChecker } from "token_wrapper";
-import AddPhoto from "components/AddPhoto";
-import Landing from "routes/Landing";
-import Main from "New/routes/Main";
-import FormsWrapper from "routes/FormsWrapper";
-import Policy from "routes/Policy";
-import Landing2 from "routes/Landing/Landing2";
-import LandingNew from "routes/LandingNew";
-import FacebookPixel from "routes/FacebookPixel";
+
+import LandingUpdated from "routes/Landing/RedOne";
 // import AddPhoto from "components/AddPhoto";
 
-export const instance = axios.create({
-  baseURL: "https://api.comabooks.org",
-});
 
-serviceOptions.axios = instance;
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 root.render(
-  <Provider store={store}>
     <BrowserRouter>
     {/* <FacebookPixel /> */}
       <Routes>
         {/* <Route path="/" element={<Landing />} />
         <Route path="/ads" element={<Landing2 />} /> */}
-        <Route path="/" element={<LandingNew />} />
-        <Route path="/login" element={<Home />} />
-        <Route path="/forms" element={<Forms />} />
-        <Route path="/forms/:id" element={<FormsWrapper />} />
-        <Route path="/addphoto/:templateId" element={<AddPhoto />} />
-        <Route path="/addphoto/:templateId/:photoId" element={<AddPhoto />} />
-        <Route path="/cover/:id" element={<CoverPage />}></Route>
-        <Route path="*" element={<Navigate to="/" replace={true} />} />
-
-        <Route path="/forms/new/:id" element={<Main />} />
-        <Route path="/policies" element={<Policy />} />
+        <Route path="/" element={<LandingUpdated />} />
       </Routes>
     </BrowserRouter>
-  </Provider>
 );
--- src/react-facebook-pixel.d.ts
+++
@@ -1,7 +0,0 @@
-declare module 'react-facebook-pixel' {
-    export function init(pixelId: string, advancedMatching?: object, options?: object): void;
-    export function pageView(): void;
-    export function track(event: string, data?: object): void;
-    export function trackCustom(event: string, data?: object): void;
-    export function fbq(...args: any[]): void;
-  }
