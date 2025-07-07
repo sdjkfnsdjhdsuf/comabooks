4f3233f1fb8c86ef3eead1ac94b04e1c6e6e65f8
sdjkfnsdjhdsuf <nurbhyt01@gmail.com>
2025-07-08 01:22:35 +0700

    info

--- src/routes/Landing/RedOne/Constructor.tsx
+++ src/routes/Landing/RedOne/Constructor.tsx
@@ -169,7 +169,7 @@ const BookConstructor: React.FC = () => {
   const handleOrder = () => {
     const message = `Здравствуйте! Я по поводу книги, можете проконсультировать ?`;
     const encodedMessage = encodeURIComponent(message);
-    const whatsappUrl = `https://wa.me/77476738427?text=${encodedMessage}`;
+    const whatsappUrl = `https://wa.me/77757962360?text=${encodedMessage}`;
     window.open(whatsappUrl, "_blank");
   };
 
