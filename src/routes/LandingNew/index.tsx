4f7731ca07b62647d702d070a693a7c302c8b188
sdjkfnsdjhdsuf <nurbhyt01@gmail.com>
2025-07-26 17:00:09 +0500

    fetch phone

--- src/routes/LandingNew/index.tsx
+++ src/routes/LandingNew/index.tsx
@@ -41,10 +41,6 @@ function Landing() {
     async function fetchPhone() {
       try {
         const res = await fetch("https://api.comabooks.org/sales/phoneNumber", {
-          headers: {
-            Authorization:
-              "fbhadbfjaebnjrnajrnjkanrkaenrkenrjeanrkeanrknarkneakrnakrn",
-          },
         });
         if (!res.ok) throw new Error("Не удалось получить номер");
         const data = await res.json();
